c19417ae0a5b89a66a5e90bfe2ad5412856e8358
julia <jujulinharizo@gamil.com>
2020-02-17 21:24:14 -0400

    primeiro commit

---
+++ aula1.py
@@ -0,0 +1,10 @@
+op=0
+print(op)
+while(op==0):
+    print('oii')
+    
+    op=int(input('bem vindo ao jogo ppt: \nSelecione uma opçao: \n[1]- jogar contra o computador\n[2] -jogar contra outro jogador\n[3] -sair do jogo\n print digite a opçao: '))
+    
+x= input('Digite a opção:')
+y= input('Digite a opção:')
+z= input('Digite a opção:')
