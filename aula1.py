9bcde70a4a1aab21d1812918a7b337fea12dc812
julia <jujulinharizo@gamil.com>
2020-02-20 21:15:13 -0400

    atualizaçao ppt (20/02)

--- aula1.py
+++ aula1.py
@@ -7,15 +7,37 @@ pc = randint(0,2)
 
 print('~'*10)
 print('escolha um adversario: \n[0] - computador\n[1] - outro jogador')
-jogador= print(int(input('qual sua opçao?: ')))
+escolha= print(int(input('qual sua opçao?: ')))
 print('~'*10)
 sleep(1)
 print('Bem vindo ao ppt\nescolha uma opçao: \n[0] - pedra\n[1] - papel\n[2] - tesoura')
+jogador= print(int(input('qual sua opçao?: ')))
 print('~'*10)
 sleep(1)
 
-if ct==0: #jogar contra o pc
-    if op==0: 
+if escolha==0: 
+    print('jogar contra o computador')
+    pc=random.randint(0,2)
+    if pc==0: 
+        if jogador==0:
+            print('EMPATE!')
+        elif jogador==1:
+            print('VOCE GANHOU!')
+        elif jogador==2:
+            print('VOCE PERDEU!')
         
-    elif op==1:
-    elif op==2:
+    elif pc==1:
+        if jogador==0:
+            print('VOCE PERDEU!')
+        elif jogador==1:
+            print('EMPATE!')
+        elif jogador==2:
+            print('VOCE GANHOU!')
+
+    elif pc==2:
+        if jogador==0:
+            print('VOCE GANHOU!')
+        elif jogador==1:
+            print('VOCE PERDEU!')
+        elif jogador==2:
+            print('EMPATE!')
